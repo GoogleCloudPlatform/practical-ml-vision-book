4249b18c25cad95367ef0079d65c0d9d5b3cd5c3
V Lakshmanan <lak@vlakshman.com>
2021-01-01 02:37:53 +0000

    Add config.yaml for different hardware configurations

---
+++ 07_training/flowers/caip_submit_job.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PACKAGE_PATH="./flowers"
+now=$(date +"%Y%m%d_%H%M%S")
+JOB_NAME="flowers_classification_$now"
+MODULE_NAME="flowers.classifier.train"
+JOB_DIR="gs://ai-analytics-solutions-kfpdemo/flowers"
+REGION='us-west1'  # make sure you have GPU/TPU quota in this region
+
+#gcloud ai-platform local train --package-path $PACKAGE_PATH --module-name $MODULE_NAME --job-dir $JOB_DIR 
+#exit
+
+# cpu only
+DISTR="cpu"
+#DISTR="gpus_one_machine"
+#DISTR="gpus_multiple_machines"
+#DISTR="tpu_caip"
+
+gcloud ai-platform jobs submit training $JOB_NAME \
+        --config ${DISTR}.yaml --region ${REGION} \
+        --package-path $PACKAGE_PATH \
+        --module-name $MODULE_NAME \
+        --job-dir $JOB_DIR \
+        --runtime-version 2.3 --python-version 3.7 \
+        -- \
+        --pattern='-*' \
+        --num_epochs=20 --distribute $DISTR
